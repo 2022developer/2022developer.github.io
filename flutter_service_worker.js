c04323f2436fa99f818c047a3088cc2a3aaedd99
2022developer <104691019+2022developer@users.noreply.github.com>
2022-08-29 23:36:45 +0530

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -58,13 +58,14 @@ const RESOURCES = {
 "assets/packages/font_awesome_flutter/lib/fonts/fa-solid-900.ttf": "dd3c4233029270506ecc994d67785a37",
 "assets/packages/wakelock_web/assets/no_sleep.js": "7748a45cd593f33280669b29c2c8919a",
 "favicon.ico": "9232540b6a38d8faf6d9e2af380e8123",
+"firebase-config.js": "97cb735f562062c97297d1ff11b6fb08",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "a76c3ab888b51d7a6fb2de6ff981a906",
-"/": "a76c3ab888b51d7a6fb2de6ff981a906",
-"main.dart.js": "98dfe87cc22a96a10e696948b04e29cb",
+"index.html": "b73736f7656bcd4cc5125aa9e84333ac",
+"/": "b73736f7656bcd4cc5125aa9e84333ac",
+"main.dart.js": "ef26731689f3e933d7f544d22ce7941a",
 "manifest.json": "9e3ca606aea05ee646b8995e02aa4f13",
 "version.json": "443dbc5ffba38a91ab8452382a5cbab5"
 };
